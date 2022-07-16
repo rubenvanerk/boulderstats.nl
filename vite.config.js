31139fd1da04317f1886cc1e91ec95bcf94dc05e
Ruben van Erk <ruben@rubenvanerk.nl>
2022-07-16 17:15:03 +0200

    Update dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
-        laravel([
-            'resources/css/app.css',
-            'resources/js/app.js',
-        ]),
-        {
-            name: 'blade',
-            handleHotUpdate({ file, server }) {
-                if (file.endsWith('.blade.php')) {
-                    server.ws.send({
-                        type: 'full-reload',
-                        path: '*',
-                    });
-                }
-            },
-        }
+        laravel({
+            input:
+                [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                ],
+            refresh: true,
+        }),
     ],
 });

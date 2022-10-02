fdaef9b3085e50ca7745f4f418b3922f5249da65
Ruben van Erk <ruben@rubenvanerk.nl>
2022-10-02 19:44:30 +0200

    Reformat stats

--- resources/views/components/stats.blade.php
+++ resources/views/components/stats.blade.php
@@ -1,12 +1,6 @@
 @props(['userIds'])
 
-<div class="grid grid-rows-5 grid-flow-col gap-x-5 overflow-y-hidden overflow-x-scroll">
-
-    <div class="px-3 py-3"></div>
-    <div class="px-3 py-3 text-xs font-medium uppercase tracking-wide text-gray-500">60d Grade</div>
-    <div class="px-3 py-3 text-xs font-medium uppercase tracking-wide text-gray-500">Tops</div>
-    <div class="px-3 py-3 text-xs font-medium uppercase tracking-wide text-gray-500">Sessions</div>
-    <div class="px-3 py-3 text-xs font-medium uppercase tracking-wide text-gray-500">Tops/Session</div>
+<div class="flex overflow-y-hidden overflow-x-scroll">
 
     @foreach($userIds as $userId)
         <livewire:user-stats :userId="$userId"/>
--- resources/views/livewire/user-stats.blade.php
+++ resources/views/livewire/user-stats.blade.php
@@ -1,36 +1,34 @@
-<div class="contents" wire:init="loadStats">
-
-    <span class="text-2xl font-bold text-gray-800 sm:text-3xl sm:truncate">{{ $user->firstName . ' ' . $user->lastName }}</span>
-    <span class="text-2xl font-extrabold text-primary-600 sm:text-3xl">
-        @if($user->stats)
-            {{ $gradeFont60d }} <sup class="text-gray-400 text-xs font-semibold">+{{ $gradeProgression60d }}%</sup>
-        @else
-            <x-placeholder/>
-        @endif
-    </span>
-    <span class="text-2xl font-extrabold text-primary-600 sm:text-3xl">
-        @if($user->ascends)
-            {{ $user?->ascends?->count() }}
-        @else
-            <x-placeholder/>
-        @endif
-    </span>
-    <span class="text-2xl font-extrabold text-primary-600 sm:text-3xl">
-        @if($user->stats)
-            {{ $user->stats->sessionCount ?: '?' }}
-        @else
-            <x-placeholder/>
-        @endif
-    </span>
-    <span class="text-2xl font-extrabold text-primary-600 sm:text-3xl">
-        @if($user->stats)
-            @if($user?->stats?->sessionCount)
-                {{ round($user?->ascends?->count() / $user?->stats?->sessionCount, 1) ?? '?' }}
-            @else
-                ?
-            @endif
-        @else
-            <x-placeholder/>
-        @endif
-    </span>
+<div wire:init="loadStats" class="flex flex-col items-center">
+    <div class="text-2xl font-bold text-gray-800 sm:text-3xl sm:truncate sticky py-2">
+        {{ $user->firstName . ' ' . $user->lastName }}
+    </div>
+    @if($readyToLoad)
+        <div class="relative py-3">
+            <div class="relative mx-auto max-w-7xl px-4">
+                <div class="mx-auto max-w-4xl">
+                    <dl class="rounded-lg bg-gray-50 shadow">
+                        <div class="flex flex-col border-b border-gray-100 p-6 text-center">
+                            <dt class="order-2 mt-1 tracking-wide text-sm font-medium text-gray-500">60d grade</dt>
+                            <dd class="order-1 text-3xl font-bold tracking-tight text-primary-600">
+                                {{ $gradeFont60d }}
+                                <sup class="text-gray-400 text-xs font-semibold">+{{ $gradeProgression60d }}%</sup>
+                            </dd>
+                        </div>
+                        <div class="flex flex-col border-t border-b border-gray-100 p-6 text-center">
+                            <dt class="order-2 mt-1 tracking-wide text-sm font-medium text-gray-500">Tops</dt>
+                            <dd class="order-1 text-3xl font-bold tracking-tight text-primary-600">{{ $user?->ascends?->count() }}</dd>
+                        </div>
+                        <div class="flex flex-col border-t border-b border-gray-100 p-6 text-center">
+                            <dt class="order-2 mt-1 tracking-wide text-sm font-medium text-gray-500">Sessions</dt>
+                            <dd class="order-1 text-3xl font-bold tracking-tight text-primary-600">{{ $user->stats->sessionCount ?: '?' }}</dd>
+                        </div>
+                        <div class="flex flex-col border-t border-gray-100 p-6 text-center">
+                            <dt class="order-2 mt-1 tracking-wide text-sm font-medium text-gray-500">Tops/session</dt>
+                            <dd class="order-1 text-3xl font-bold tracking-tight text-primary-600">{{ round($user?->ascends?->count() / $user?->stats?->sessionCount, 1) ?? '?' }}</dd>
+                        </div>
+                    </dl>
+                </div>
+            </div>
+        </div>
+    @endif
 </div>

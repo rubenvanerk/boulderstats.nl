56b894e1e88d40f0e2d34645cc390ef20a16fa7c
Ruben van Erk <ruben@rubenvanerk.nl>
2022-11-27 13:56:08 +0100

    Only show first name

--- resources/views/livewire/user-stats.blade.php
+++ resources/views/livewire/user-stats.blade.php
@@ -1,6 +1,6 @@
 <div wire:init="loadStats" class="flex flex-col items-center pb-2 w-40">
     <div class="text-2xl font-bold text-gray-800 sm:text-3xl sm:truncate sticky top-0 py-2 flex items-center">
-        {{ $user->firstName }} {{ $user->lastName }}
+        {{ $user->firstName }}
         <x-icons.x-circle wire:click="remove" class="block ml-2 h-6 w-6 hover:text-gray-500 hover:cursor-pointer"></x-icons.x-circle>
     </div>
 
